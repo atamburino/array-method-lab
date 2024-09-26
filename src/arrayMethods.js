10c2c3adde4ded3f28cdabdebb85f4dfdb250b63
Andy Tamburino <andytamburino@gmail.com>
2024-09-26 13:58:10 -0400

    testing helper function

--- src/arrayMethods.js
+++ src/arrayMethods.js
@@ -1,6 +1,19 @@
-function swapFirstAndLast(inputArray) {
+// Helper function to validate input
+function isValidArray(inputArray) {
+    if (inputArray.length === 0) {
+        console.log(`Array is empty:${inputArray}`)
+        return 'Invalid input';
+    }
     if (!Array.isArray(inputArray) || inputArray.length < 2) {
         return 'Invalid input';
+    }
+    return true;
+}
+
+function swapFirstAndLast(inputArray) {
+    const validationResult = isValidArray(inputArray);
+    if (validationResult !== true) {
+        return validationResult; // Returns Invalid Input
     }
 
     let firstElement = inputArray[0];
